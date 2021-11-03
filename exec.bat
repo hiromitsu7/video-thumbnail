fea3a00b0fe1ff042ee414fa7e1f4cb22dac93f8
Hiromitsu Iwata <hiromitsu7@gmail.com>
2021-11-03 23:50:24 +0900

    windows

--- exec.bat
+++ exec.bat
@@ -1,7 +1,8 @@
 C:\github\video-thumbnail\video-thumbnail.exe C:\Users\hiro\Downloads
 
-REM MOVE /Y C:\Users\hiro\Downloads\*.gif D:\mov\img\
-REM MOVE /Y C:\Users\hiro\Downloads\*.txt D:\mov\ffprobe\
-REM MOVE /Y C:\Users\hiro\Downloads\*.mp4 D:\mov\downloads\
-REM MOVE /Y C:\Users\hiro\Downloads\*.wmv D:\mov\downloads\
+REM コマンドの中でmvする
+REM Move-Item C:\Users\hiro\Downloads\*.gif D:\mov\img\
+REM Move-Item C:\Users\hiro\Downloads\*.txt D:\mov\ffprobe\
+REM Move-Item C:\Users\hiro\Downloads\*.mp4 D:\mov\download\
+REM Move-Item C:\Users\hiro\Downloads\*.wmv D:\mov\download\
 
