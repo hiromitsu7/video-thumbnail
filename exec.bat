21462b5d0bdb9c9aacfd98089645b4629be43bf0
Hiromitsu Iwata <hiromitsu7@gmail.com>
2021-11-07 23:01:19 +0900

    不要なファイルを削除

--- exec.bat
+++
@@ -1,8 +0,0 @@
-C:\github\video-thumbnail\video-thumbnail.exe C:\Users\hiro\Downloads
-
-REM コマンドの中でmvする
-REM Move-Item C:\Users\hiro\Downloads\*.gif D:\mov\img\
-REM Move-Item C:\Users\hiro\Downloads\*.txt D:\mov\ffprobe\
-REM Move-Item C:\Users\hiro\Downloads\*.mp4 D:\mov\download\
-REM Move-Item C:\Users\hiro\Downloads\*.wmv D:\mov\download\
-
